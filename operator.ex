2053a81efdce4007137a5c14b51a8979eec6ea14
unknown <A01734881@tec.mx>
2022-06-10 21:51:03 -0500

    Pruebas corregidas

--- operator.ex
+++ operator.ex
@@ -4,6 +4,7 @@ lista = Enum.map(number, fn x -> "prueba" <> Integer.to_string(x) <> ".py" end)
 Path.expand(File.cwd!,"~/pruebas")
 
 IO.inspect(lista)
+
 Benchee.run(
   %{
     "concurrente" => fn -> Resaltador.concurrente(lista) end,
